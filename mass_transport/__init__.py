d57013fa087f0343fa66b2fb7ecc17901cfe7743
Kyle Treleaven <treleaven.kyle@gmail.com>
2013-06-27 17:39:00 -0400

    god I hope this works

---
+++ mass_transport/__init__.py
@@ -0,0 +1,25 @@
+
+# got this code from StackOverflow:
+# http://stackoverflow.com/questions/5137082/django-python-modules-and-git-submodules
+
+
+#At the top of settings.py
+import sys, os
+git_sub_modules = 'packages'     #Relative paths ok too
+
+this_dir, this_file = os.path.split( __file__ )
+git_sub_modules_abs = os.path.join( this_dir, git_sub_modules )
+
+for dir in os.listdir( git_sub_modules_abs ) :
+    path = os.path.join( git_sub_modules_abs, dir )
+    if not path in sys.path :
+        sys.path.append( path )
+        
+        
+        
+""" imports """
+
+import road_Ed
+import roademd
+import roademd_approx1
+import roademd_approx2
