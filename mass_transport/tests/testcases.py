c783e6be2d053c2cc6c4f30b1299a764c98a64d5
Kyle Treleaven <treleaven.kyle@gmail.com>
2013-06-28 15:07:47 -0400

    adding some testcases... git status wasn't reporting these new files...

---
+++ mass_transport/tests/testcases.py
@@ -0,0 +1,17 @@
+
+import networkx as nx
+
+
+
+class RoadnetExamples :
+    @classmethod
+    def get(cls, key ) :
+        roadnet = nx.MultiDiGraph()
+        
+        if key == 'nesw' :
+            roadnet.add_edge( 0, 1, 'N', length=1., weight2=3. )
+            roadnet.add_edge( 1, 2, 'E', length=2., weight1=1000., oneway=True )
+            roadnet.add_edge( 2, 3, 'S', length=1., weight1=1. )
+            roadnet.add_edge( 3, 0, 'W', length=1., weight2=1. )
+            
+        return roadnet
