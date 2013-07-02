7539a74fd5aed67b7e4a04b15337ee1fc55b5905
Kyle Treleaven <treleaven.kyle@gmail.com>
2013-07-01 21:42:28 -0400

    removing confounding 'weights' and 'oneway-ness' from testcase

--- mass_transport/tests/testcases.py
+++ mass_transport/tests/testcases.py
@@ -9,9 +9,13 @@ class RoadnetExamples :
         roadnet = nx.MultiDiGraph()
         
         if key == 'nesw' :
-            roadnet.add_edge( 0, 1, 'N', length=1., weight2=3. )
-            roadnet.add_edge( 1, 2, 'E', length=2., weight1=1000., oneway=True )
-            roadnet.add_edge( 2, 3, 'S', length=1., weight1=1. )
-            roadnet.add_edge( 3, 0, 'W', length=1., weight2=1. )
+            #roadnet.add_edge( 0, 1, 'N', length=1., weight2=3. )
+            #roadnet.add_edge( 1, 2, 'E', length=2., weight1=1000., oneway=True )
+            #roadnet.add_edge( 2, 3, 'S', length=1., weight1=1. )
+            #roadnet.add_edge( 3, 0, 'W', length=1., weight2=1. )
+            roadnet.add_edge( 0, 1, 'N', length=1. )
+            roadnet.add_edge( 1, 2, 'E', length=2. )
+            roadnet.add_edge( 2, 3, 'S', length=1. )
+            roadnet.add_edge( 3, 0, 'W', length=1. )
             
         return roadnet
