328be00558044365d5c307067e296f4b615450bb
Kyle Treleaven <treleaven.kyle@gmail.com>
2013-07-08 16:07:39 -0400

    tracking a bug fix in nxopt, and adding a EMD-sanity-checking test case to the self-test script of transportation_complexity

--- mass_transport/transport_complexity.py
+++ mass_transport/transport_complexity.py
@@ -141,75 +141,93 @@ def display( graph, coord='coord' ) :
     nx.draw( graph, pos=layout )
     
 
-if True :
+
+if __name__ == '__main__' :
     GRAPH = nx.DiGraph()
     
-    PLACES = [ 'HOME', 'WORK', 'SHOPPING' ]
-    #PLACES = [ 'HOME', 'WORK', 'SHOPPING', 'GYM', 'MOVIE' ]
-    for place in PLACES :
-        GRAPH.add_node( place, coord=randompoint() )
+    if False :
+        GRAPH.add_edge(0,2,length=5.)
+        GRAPH.add_edge(1,3,length=1.)
+        GRAPH.add_edge(0,3,length=1.)
+        GRAPH.add_edge(3,2,length=1.)
         
-    for o, o_data in GRAPH.nodes_iter( data=True ) :
-        o_loc = o_data.get( 'coord' )
+        GRAPH.node[0]['weight1'] = 1.
+        GRAPH.node[1]['weight1'] = 1.
+        GRAPH.node[2]['weight2'] = 1.
+        GRAPH.node[3]['weight2'] = 1.
         
-        for d, d_data in GRAPH.nodes_iter( data=True ) :
-            #if d is o : continue
-            # this is actually important, max_flow_min_cost chokes without it
-            
-            # geometry
-            d_loc = d_data.get( 'coord' )
-            distance = np.linalg.norm( d_loc - o_loc )
-            
-            # demand
-            rate = np.random.exponential()
-            
-            GRAPH.add_edge( o, d, distance=distance, rate=rate )
-            
-    enroute_velocity = demand_enroute_velocity( GRAPH, GRAPH, length='distance' )
-    balance_velocity = demand_balance_velocity( GRAPH, GRAPH, length='distance' )
-    
-    #flowgraph, costgraph = obtainWassersteinProblem( DEMAND, DEMAND, length='distance' )
-    
-    print enroute_velocity, balance_velocity
-    
-    
-    distance_between = lambda u, v : GRAPH.get_edge_data( u, v ).get('distance')
-    
-    class demand :
-        def __init__(self, p, q ) :
-            self.pick = p
-            self.delv = q
-            
-    T = 20.
-    DEMANDS = []
-    for u, v, data in GRAPH.edges_iter( data=True ) :
-        n = np.random.poisson( data.get('rate') * T )
-        DEMANDS.extend([ demand(u,v) for i in range(n) ])
         
-    total_enroute = sum( [ distance_between( dem.pick, dem.delv ) for dem in DEMANDS ] )
-    mean_enroute = total_enroute / T
-    
-    
-    
-    if True :
-        graph = nx.Graph()
-        for dem1, dem2 in itertools.product( DEMANDS, DEMANDS ) :
-            #if dem2 is dem1 : continue  # those edges aren't in there
-            
-            dist = distance_between( dem1.delv, dem2.pick )
-            graph.add_edge( (dem1,0), (dem2,1), weight = -dist )    # about to do MAX_weight_matching
-                
-        mate = nx.matching.max_weight_matching( graph, maxcardinality=True )
-        total_balance = sum( [ distance_between( dem1.delv, dem2.pick )
-                              for ( (dem1,side1), (dem2,side2) ) in mate.iteritems() if side1 == 0 ] )
-        mean_balance = total_balance / T
+        print 'EMD: %f' % EarthMoversDistance( GRAPH, GRAPH )
+        print 'the answer should be 3. (pretty sure.)'
+
     else :
-        mean_balance = None
+        PLACES = [ 'HOME', 'WORK', 'SHOPPING' ]
+        #PLACES = [ 'HOME', 'WORK', 'SHOPPING', 'GYM', 'MOVIE' ]
+        for place in PLACES :
+            GRAPH.add_node( place, coord=randompoint() )
+            
+        for o, o_data in GRAPH.nodes_iter( data=True ) :
+            o_loc = o_data.get( 'coord' )
+            
+            for d, d_data in GRAPH.nodes_iter( data=True ) :
+                #if d is o : continue
+                # this is actually important, max_flow_min_cost chokes without it
+                
+                # geometry
+                d_loc = d_data.get( 'coord' )
+                distance = np.linalg.norm( d_loc - o_loc )
+                
+                # demand
+                rate = np.random.exponential()
+                
+                GRAPH.add_edge( o, d, distance=distance, rate=rate )
     
-    print mean_enroute, mean_balance
+        enroute_velocity = demand_enroute_velocity( GRAPH, GRAPH, length='distance' )
+        balance_velocity = demand_balance_velocity( GRAPH, GRAPH, length='distance' )
+        
+        #flowgraph, costgraph = obtainWassersteinProblem( DEMAND, DEMAND, length='distance' )
+        
+        print enroute_velocity, balance_velocity
+        
     
-    
-    
-    
+        
+        distance_between = lambda u, v : GRAPH.get_edge_data( u, v ).get('distance')
+        
+        class demand :
+            def __init__(self, p, q ) :
+                self.pick = p
+                self.delv = q
+                
+        T = 20.
+        DEMANDS = []
+        for u, v, data in GRAPH.edges_iter( data=True ) :
+            n = np.random.poisson( data.get('rate') * T )
+            DEMANDS.extend([ demand(u,v) for i in range(n) ])
+            
+        total_enroute = sum( [ distance_between( dem.pick, dem.delv ) for dem in DEMANDS ] )
+        mean_enroute = total_enroute / T
+        
+        
+        
+        if True :
+            graph = nx.Graph()
+            for dem1, dem2 in itertools.product( DEMANDS, DEMANDS ) :
+                #if dem2 is dem1 : continue  # those edges aren't in there
+                
+                dist = distance_between( dem1.delv, dem2.pick )
+                graph.add_edge( (dem1,0), (dem2,1), weight = -dist )    # about to do MAX_weight_matching
+                    
+            mate = nx.matching.max_weight_matching( graph, maxcardinality=True )
+            total_balance = sum( [ distance_between( dem1.delv, dem2.pick )
+                                  for ( (dem1,side1), (dem2,side2) ) in mate.iteritems() if side1 == 0 ] )
+            mean_balance = total_balance / T
+        else :
+            mean_balance = None
+        
+        print mean_enroute, mean_balance
+        
+            
+        
+        
     
     
