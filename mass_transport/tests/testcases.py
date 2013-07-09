5c4de93639f5aea1b7fae584f047635f4ac1b379
Kyle Treleaven <treleaven.kyle@gmail.com>
2013-07-09 08:37:55 -0400

    making road lengths 1 to agree with the paper

--- mass_transport/tests/testcases.py
+++ mass_transport/tests/testcases.py
@@ -14,7 +14,7 @@ class RoadnetExamples :
             #roadnet.add_edge( 2, 3, 'S', length=1., weight1=1. )
             #roadnet.add_edge( 3, 0, 'W', length=1., weight2=1. )
             roadnet.add_edge( 0, 1, 'N', length=1. )
-            roadnet.add_edge( 1, 2, 'E', length=2. )
+            roadnet.add_edge( 1, 2, 'E', length=1. )    # was 2. before
             roadnet.add_edge( 2, 3, 'S', length=1. )
             roadnet.add_edge( 3, 0, 'W', length=1. )
             
